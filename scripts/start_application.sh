fb8cd5d859c107f0dc70c54505c76dbcf10882c8
Alisson Machado <aamachado@inf.ufsm.br>
2024-11-16 18:56:02 -0300

    add enviroment variables

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -9,6 +9,8 @@ cd /opt/portfolio_landing
 # Export required environment variables
 export PORT=4000
 export MIX_ENV=prod
+export PHX_HOST=alissonmachado.dev
+export PHX_SERVER=true
 export SECRET_KEY_BASE=$(aws ssm get-parameter --name "/portfolio_landing/prod/secret_key_base" --with-decryption --query Parameter.Value --output text)
 export DATABASE_URL=$(aws ssm get-parameter --name "/portfolio_landing/prod/database_url" --with-decryption --query Parameter.Value --output text)
 
