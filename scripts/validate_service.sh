4ddc50130cf5dc6cb400305867673c215965e93f
Alisson Machado <aamachado@inf.ufsm.br>
2024-11-16 12:50:22 -0300

    fix: aws CodeDeploy

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-# Wait for the application to be responsive
-for i in {1..30}; do
-    if curl -s http://localhost:4000 > /dev/null; then
-        echo "Service is running"
-        exit 0
-    fi
-    echo "Waiting for service to start..."
-    sleep 2
-done
-echo "Service failed to start"
-exit 1
+# Is this running?
+systemctl is-active portfolio_landing.service
+
+# Is this answering?
+timeout 30 bash -c 'until printf "" 2>>/dev/null >>/dev/tcp/localhost/4000; do sleep 1; done'
+
+
+exit 0
